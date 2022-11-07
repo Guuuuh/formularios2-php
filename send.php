36bd48e6a914e5f19dc0d19b5342665a81736a1d
Gustavo H. Tomaz Lopes <gustavo.htl@hotmail.com>
2022-11-07 15:29:56 -0300

    token csrf e fazendo um captcha

--- send.php
+++ send.php
@@ -5,5 +5,5 @@ session_start();
 include 'libs/captcha.php';
 include 'libs/csrf.php';
 include 'libs/get_data.php';
-include 'libs/url.php';
-include 'libs/mail.php';
+//include 'libs/url.php';
+//include 'libs/mail.php';
